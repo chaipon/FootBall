0586a81b5e8cd92f778d190af587e37a540b8f3d
Minoru Kaneko <mekun@be.to>
2013-09-25 00:09:49 +0900

    goal indexのテストを修正。少し詳細に。。

--- spec/views/goals/index.html.erb_spec.rb
+++ spec/views/goals/index.html.erb_spec.rb
@@ -1,19 +1,22 @@
 require 'spec_helper'
 
 describe "goals/index" do
+  fixtures :games, :players, :teams
   before(:each) do
     assign(:goals, [
       stub_model(Goal,
-        :time => 1,
-        :game_id => nil,
-        :player_id => nil,
-        :is_our_goal => ""
+        :time => 10,
+        :game_id => 1,
+        :player_id => 3,
+        :is_our_goal => "true",
+        :created_at => DateTime.parse("2013/10/1 20:00")
       ),
       stub_model(Goal,
-        :time => 1,
-        :game_id => nil,
-        :player_id => nil,
-        :is_our_goal => ""
+        :time => 11,
+        :game_id => 2,
+        :player_id => 4,
+        :is_our_goal => "true",
+        :created_at => DateTime.parse("2013/10/1 20:00")
       )
     ])
   end
@@ -21,9 +24,12 @@ describe "goals/index" do
   it "renders a list of goals" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
+    assert_select "tr>td", :text => 10.to_s, :count => 1
+    assert_select "tr>td", :text => 11.to_s, :count => 1
+    assert_select "tr>td", :text => 4.to_s, :count => 1
+    assert_select "tr>td", :text => 3.to_s, :count => 1
+    assert_select "tr>td", :text => 1.to_s, :count => 1
+    assert_select "tr>td", :text => 2.to_s, :count => 1
+    assert_select "tr>td", :text => "true", :count => 2
   end
 end
