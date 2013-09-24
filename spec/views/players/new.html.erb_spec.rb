a96708389f42aa1e9b1da24f755ae75d3e26b548
Minoru Kaneko <mekun@be.to>
2013-09-25 00:46:07 +0900

    テスト追加 (pending解除)

--- spec/views/players/new.html.erb_spec.rb
+++ spec/views/players/new.html.erb_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe "players/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :teams
+  before(:each) do
+    assign(:player, stub_model(Player,
+      :name => nil,
+    ).as_new_record)
+    @teams = Team.all
+  end
+
+  it "renders new player form" do
+    render
+
+    assert_select "form[action=?][method=?]", players_path, "post" do
+      assert_select "input#player_name[name=?]", "player[name]"
+      assert_select "input#player_password[name=?]", "player[password]"
+      assert_select "input#player_password_confirmation[name=?]", "player[password_confirmation]"
+      assert_select "select#player_team_id[name=?]", "player[team_id]" do
+        assert_select "option[value=?]", "1", "Bears" 
+        assert_select "option[value=?]", "2", "Sting" 
+      end
+    end
+
+  end
 end
